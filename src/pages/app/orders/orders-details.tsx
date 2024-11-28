0c65fe7013e865d2f2b329eda4acfcf147aeadf9
João Trajano <joao.trajanosouza@gmail.com>
2024-11-28 12:53:15 -0300

    feat: implementing order details

---
+++ src/pages/app/orders/orders-details.tsx
@@ -0,0 +1,67 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+
+export function OrdersDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedio: j12nwce9hfweweriqw09-euir</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+                  <span className="font-mediu text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">
+                João Trajano de Souza Neto
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">12312312312</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-mediu text-muted-foreground">
+                    joao.trajanosouza@gmail.com
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="font-mediu text-muted-foreground">
+                    há 3 minutos
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
