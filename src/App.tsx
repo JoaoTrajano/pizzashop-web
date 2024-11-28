0842804e9637c5dced9f0ad5aac0f72b5b7e7ce3
João Trajano <joao.trajanosouza@gmail.com>
2024-11-28 09:32:10 -0300

    feat: added form and toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors position="top-center" />
       <Helmet titleTemplate="%s | Pizza Shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,37 @@
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
 import { ContentPage } from '@/components/ContentPage'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+type SignInFormType = z.infer<typeof signInFormSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormType>()
+
+  function handleSignIn(data: SignInFormType) {
+    try {
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
+  }
+
   return (
     <ContentPage titlePage="Login">
       <div className="p-8">
@@ -16,15 +44,15 @@ export function SignIn() {
               Acompanhe as suas vendas pelo painel do parceiro!
             </p>
           </div>
-          <div className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email"> Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar Painel
             </Button>
-          </div>
+          </form>
         </div>
       </div>
     </ContentPage>
